ee4d67f9d50342fd87eabb324f3f95d12230090e
Ryan Bates <rebates@WIN2.CS.KSU.EDU>
2017-08-28 11:00:26 -0500

    8/25 classwork

--- checkers.js
+++ checkers.js
@@ -151,8 +151,62 @@ function checkLanding(moves, jumps, piece, cx, cy, lx, ly) {
   * A function to apply the selected move to the game
   * @param {object} move - the move to apply.
   */
-function applyMove(move) {
+function applyMove(x, y, move) {
   // TODO: Apply the move
-  // TODO: Check for victory
-  // TODO: Start the next turn
+  if(move.type === 'slide'){
+	  state.board[move.y][move.x] = state.board[y][x];  
+	  state.board[y][x] = null;
+  }
+  else{
+	move.captures.forEach(function(square){
+		state.board[square.y][square.x] = null;
+	});
+	var index = move.landings.length - 1;
+	state.board[move.landings[index].y][move.landings[index].x] = state.board[y][x];
+	 state.board[y][x] = null;
+  }
 }
+
+  
+// TODO: Check for victory
+function checkForVictory(){
+  var wCount = 0;
+  var bCount = 0;
+  for(y = 0; y < 10; y++)
+  {
+	for(x = 0; y < 10; x++)
+	{
+	  if(state.board[y][x] === "w" || state.board[y][x] === "w"){
+		  wCount++;
+	  }
+	  if(state.board[y][x] === "b" || state.board[y][x] === "b"){
+		  bCount++;
+	  }
+	}
+  }
+  if(wCount == 0){
+	  state.over = true;
+	 return "Black wins";
+  }
+  if(bCount == 0){
+	  state.over = true;
+	  return "White wins";
+  }
+  return false;
+}
+  
+// TODO: Start the next turn
+function nextTurn(){
+		if(state.turn === 'b') state.turn = 'w';
+		else state.turn = 'b';
+}
+
+function printBoard(){
+	console.log(state.board);
+}
+
+function main(){
+	printBoard();
+}
+
+main();
